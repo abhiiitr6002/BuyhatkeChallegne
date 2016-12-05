82bb63d67b6b71e05c0c2c94afefe077f77fcdbe
abhiiitr6002 <abhiiitr6002@gmail.com>
2016-12-06 04:07:50 +0530

    Complete application

---
+++ app/src/main/java/com/example/abhishek/buyhatkechallenge/helper/SmsReciever.java
@@ -0,0 +1,63 @@
+package com.example.abhishek.buyhatkechallenge.helper;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.telephony.SmsMessage;
+
+import com.example.abhishek.buyhatkechallenge.MainActivity;
+import com.example.abhishek.buyhatkechallenge.R;
+import com.example.abhishek.buyhatkechallenge.UI.ReceiveSMSActivity;
+
+/**
+ * Created by abhishek on 5/12/16.
+ */
+public class SmsReciever extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] smsm = null;
+            String sms_str = "";
+            String sender = "";
+            if (bundle != null) {
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                smsm = new SmsMessage[pdus.length];
+                int id = 1;
+                for (int i = 0; i < 1; i++) {
+                    smsm[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    sms_str += "Sent From: " + smsm[i].getOriginatingAddress();
+                    sender = smsm[i].getDisplayOriginatingAddress();
+
+                    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
+                            .setContentTitle(sender)
+                            .setContentText(sms_str)
+                            .setSmallIcon(R.drawable.qwqw);
+                    Intent in = new Intent (context, ReceiveSMSActivity.class);
+                    in.addFlags (Intent.FLAG_ACTIVITY_NEW_TASK);
+                    PendingIntent pend = PendingIntent.getActivity(context, 0, in, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                    NotificationManager notificationManager =
+                            (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                    notificationBuilder.setContentIntent (pend);
+                    try{
+                    notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+                    }catch(Exception e){
+                        e.printStackTrace();
+                        e.toString();
+                    }
+                }
+            }
+        }
+    }
+
+
+}
